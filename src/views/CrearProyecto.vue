ad57db4c5534acc766a122ab7ddd7a6c9c4be5fb
Juan Camilo Davila R <jcamilodavila25@gmail.com>
2025-09-21 15:27:39 -0500

    Actualizando

--- src/views/CrearProyecto.vue
+++ src/views/CrearProyecto.vue
@@ -1,35 +1,40 @@
 <template>
   <q-page padding class="creacion-qr">
     <h1>Creación de un Proyecto con Vite</h1>
-    <p>
-      La forma más común de iniciar un proyecto moderno de Vue 3 es utilizando Vite,
-      un bundler rápido y ligero.
-    </p>
-    <ol>
-      <li>Lo primero es crear un proyecto Vue con Vite. Ejecuta <code>npm create vite@latest mi-proyecto</code></li>
-      <div class="imagenes-qr">
-        <img src="../assets/crearProyecto1.jpeg" alt="crearProyecto1" class="imagen">
+     <p>
+          La forma más común de iniciar un proyecto moderno de Vue 3 es utilizando Vite,
+          un bundler rápido y ligero.
+        </p>
+    <div class="main-container">
+      <div>
+       
+        <ol>
+          <li>Lo primero es crear un proyecto Vue con Vite. Ejecuta <code>npm create vite@latest mi-proyecto</code></li>
+          <div class="imagenes-qr">
+            <img src="../assets/crearProyecto1.jpeg" alt="crearProyecto1" class="imagen">
+          </div>
+          <li>Selecciona la plantilla <b>Vue</b> o <b>Vue + JavaScript</b>. </li>
+          <div class="imagenes-qr">
+            <img src="../assets/captura.jpg" alt="seleccionarPlantilla" class="imagen">
+          </div>
+          <li>Luego entra a la carpeta e instala las dependencias iniciales con: <code>npm install</code></li>
+          <div class="imagenes-qr">
+            <img src="../assets/ingresarCarpeta.jpg" alt="ingresarCarpeta" class="imagen">
+          </div>
+          <li>Ejecuta <code>npm run dev</code> para iniciar el servidor local.</li>
+          <div class="imagenes-qr">
+            <img src="../assets/ejecutarServidor.jpg" alt="iniciarServidor" class="imagen">
+          </div>
+          <li>La terminal debe mostrar algo como:</li>
+          <div class="imagenes-qr">
+            <img src="../assets/mostrarTerminal.jpg" alt="mostrarTerminal" class="imagen">
+          </div>
+        </ol>
       </div>
-      <li>Selecciona la plantilla <b>Vue</b> o <b>Vue + JavaScript</b>. </li>
-      <div class="imagenes-qr">
-        <img src="../assets/captura.jpg" alt="seleccionarPlantilla" class="imagen">
-      </div>
-      <li>Luego entra a la carpeta e instala las dependencias iniciales con: <code>npm install</code></li>
-      <div class="imagenes-qr">
-        <img src="../assets/ingresarCarpeta.jpg" alt="ingresarCarpeta" class="imagen">
-      </div>
-      <li>Ejecuta <code>npm run dev</code> para iniciar el servidor local.</li>
-      <div class="imagenes-qr">
-        <img src="../assets/ejecutarServidor.jpg" alt="iniciarServidor" class="imagen">
-      </div>
-      <li>La terminal debe mostrar algo como:</li>
-      <div class="imagenes-qr">
-        <img src="../assets/mostrarTerminal.jpg" alt="mostrarTerminal" class="imagen">
-      </div>
-    </ol>
-    <p>La estructura mínima recomendada debe tener lo siguiente:</p>
+      <div class="container02">
+        <p style="font-size: 15px; font-family: 'prototype', sans-serif; padding-top: 10px; padding-right: 10px;">La estructura mínima recomendada debe tener lo siguiente:</p>
 
-    <pre>
+        <pre>
       │
       ├─ public/ 
       │ └─ index.html
@@ -50,6 +55,8 @@
       ├─ vite.config.js
       └─ README.md
     </pre>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -58,6 +65,12 @@
 @font-face {
   font-family: 'prototype';
   src: url('../fonts/Prototype.ttf') format('truetype');
+}
+
+.main-container {
+  display: flex;
+  justify-content: space-between;
+  margin-right: 100px;
 }
 
 .creacion-qr h1 {
@@ -81,7 +94,6 @@
 .imagenes-qr img {
   width: 40rem;
   filter: drop-shadow(2px 4px 6px black);
-  /* margin-left: 10px; */
   transition: transform 0.3s ease;
   margin-bottom: 10px;
 }
--- src/views/introduccion.vue
+++ src/views/introduccion.vue
@@ -9,19 +9,21 @@
       Vue destaca por su simplicidad, su curva de aprendizaje accesible y su potente ecosistema,
       que incluye librerías como:
     </p>
-    <ul>
+    
+    <div class="main-container">
+      <ul>
       <li><b>Vue Router</b> (navegación)</li>
       <li><b>Pinia</b> (gestión de estado)
         y frameworks UI como </li>
       <li><b>Quasar</b>.</li>
     </ul>
-
-    <div class="video-container">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/nhBVL41-_Cw?si=G_GXlvlRFq2bFRRx"
-        title="Vue.js Explained in 100 Seconds" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-      </iframe>
+      <div class="video-container">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/nhBVL41-_Cw?si=G_GXlvlRFq2bFRRx"
+          title="Vue.js Explained in 100 Seconds" frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+        </iframe>
+      </div>
     </div>
 
   </q-page>
@@ -45,7 +47,7 @@
 
 .introduccion-qr p {
   font-size: 16px;
-  margin: 20px;
+  margin: 20px 80px 10px 20px;
   font-family: sans-serif;
   font-weight: 300;
   text-align: justify;
@@ -54,6 +56,11 @@
 .introduccion-qr ul {
   font-family: sans-serif;
   font-size: 16px;
+}
+
+.main-container {
+  display: flex;
+  justify-content: space-between;
 }
 
 .video-container {
